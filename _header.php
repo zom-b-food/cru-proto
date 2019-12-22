33b31374c4a19d05210f68904a2f246e478739ea
zom-b-food <am@ui-design-engineering.com>
2019-12-22 11:02:43 -0800

    new images

--- _header.php
+++ _header.php
@@ -2,7 +2,7 @@
 	alt="Claims Processing and Eligibility - CHAMPVA Regulatory Updates - CRU - VA|U.S. Department of Veterans Affairs"
 	title="Claims Processing and Eligibility - CHAMPVA Regulatory Updates - CRU - VA|U.S. Department of Veterans Affairs"
 	src="/dist/img/cru-banner.jpg">
-<div class="col-12 mt25">
+<div class="col-12 mt15">
 	<small>
 		<a class="btn btn-back" role="button" href="landing-page.php" tabindex=1>&nbsp;
 			<i class="fas fa-angle-left"></i>&nbsp;DCS Home
